7f17d8be44b7e1be29c67a6e1bcae974b4dc70dc
Beck <fazlidin.00@mail.ru>
2024-05-31 10:29:11 +0300

    start project

---
+++ src/test/java/com/umaraliev/FakePaymentProvider/FakePaymentProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umaraliev.FakePaymentProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakePaymentProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
